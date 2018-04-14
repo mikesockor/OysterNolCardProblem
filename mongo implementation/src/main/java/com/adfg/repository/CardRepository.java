6b4fe32f82ed226d51e47a12a70e8b9cf3bd77bc
sockor <zdobwork@gmail.com>
2018-04-14 17:58:40 +0400

    @Repository for autowaring

--- mongo implementation/src/main/java/com/adfg/repository/CardRepository.java
+++ mongo implementation/src/main/java/com/adfg/repository/CardRepository.java
@@ -2,11 +2,13 @@ package com.adfg.repository;
 
 import com.adfg.domain.Card;
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
+import org.springframework.stereotype.Repository;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 import java.util.Date;
 
+@Repository
 public interface CardRepository extends ReactiveMongoRepository<Card, String> {
     Mono<Card> findById(String id);
 
--- mongo implementation/src/main/java/com/adfg/repository/TransactionRepository.java
+++ mongo implementation/src/main/java/com/adfg/repository/TransactionRepository.java
@@ -2,10 +2,12 @@ package com.adfg.repository;
 
 import com.adfg.domain.Transaction;
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
+import org.springframework.stereotype.Repository;
 import reactor.core.publisher.Flux;
 
 import java.util.Date;
 
+@Repository
 public interface TransactionRepository extends ReactiveMongoRepository<Transaction, String> {
     Flux<Transaction> findByCheckInTimeGreaterThanAndCardId(Date date, String id);
 }
